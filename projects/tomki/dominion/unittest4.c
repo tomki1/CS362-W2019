e2da012f5a9e7737a3627bea90903c8da838bc17
tomki1 <38170380+tomki1@users.noreply.github.com>
2019-02-06 20:46:01 -0800

    Update unittest4.c

--- projects/tomki/dominion/unittest4.c
+++ projects/tomki/dominion/unittest4.c
@@ -1,7 +1,4 @@
 /* -----------------------------------------------------------------------
- * Demonstration of how to write unit tests for dominion-base
- * Include the following lines in your makefile:
- *
  * unittest4: unittest4.c dominion.o rngs.o
  *      gcc -o unittest4 -g  unittest4.c dominion.o rngs.o $(CFLAGS)
  * -----------------------------------------------------------------------
