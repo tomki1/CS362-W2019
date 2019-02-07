17c560d1bf6f42ca2beae4951b8166a278a93601
tomki1 <38170380+tomki1@users.noreply.github.com>
2019-02-06 20:44:01 -0800

    Update cardtest4.c

--- projects/tomki/dominion/cardtest4.c
+++ projects/tomki/dominion/cardtest4.c
@@ -1,7 +1,4 @@
 /* -----------------------------------------------------------------------
- * Demonstration of how to write unit tests for dominion-base
- * Include the following lines in your makefile:
- *
  * cardtest4: cardtest4.c dominion.o rngs.o
  *      gcc -o cardtest4 -g  cardtest4.c dominion.o rngs.o $(CFLAGS)
  * -----------------------------------------------------------------------
